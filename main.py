645b482e9c74a78294248e98896a0291e5f73b83
sillypantscoder <78504018+sillypantscoder@users.noreply.github.com>
2022-11-30 15:10:42 +0000

    Basic Pygame starter script

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ import pygame
 
 SCREENSIZE = [640, 480]
 screen = pygame.display.set_mode(SCREENSIZE, pygame.RESIZABLE)
+
+c = pygame.time.Clock()
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		elif event.type == pygame.VIDEORESIZE:
+			SCREENSIZE = event.size
+			screen = pygame.display.set_mode(SCREENSIZE, pygame.RESIZABLE)
+	screen.fill((255, 255, 255))
+	pygame.display.flip()
+	c.tick(60)
